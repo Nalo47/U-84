c44b6e2665394faef598167597238a0970388ee6
Sercan Ümit Öner <117740824+lucanusx@users.noreply.github.com>
2023-06-26 16:33:31 +0300

    added player combat mechanic

--- Assets/Scripts/IKLook.cs
+++ Assets/Scripts/IKLook.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 
 public class IKLook : MonoBehaviour
 {
+    private float weight;
     private Animator _anim;
     private Camera mainCam;
     
@@ -16,8 +17,18 @@ public class IKLook : MonoBehaviour
 
     private void OnAnimatorIK(int layerIndex)
     {
-        _anim.SetLookAtWeight(1f,.5f,1.2f,.5f,.5f);
+        _anim.SetLookAtWeight(weight,.5f,1.2f,.5f,.5f);
         Ray lookAtRay = new Ray(transform.position, mainCam.transform.forward);
         _anim.SetLookAtPosition(lookAtRay.GetPoint(25));
+    }
+
+    public void art()
+    {
+        weight = Mathf.Lerp(weight, 1f, Time.fixedDeltaTime);
+    }
+
+    public void azal()
+    {
+        weight = Mathf.Lerp(weight, 0f, Time.fixedDeltaTime);
     }
 }
--- Assets/Scripts/weaponController.cs
+++ Assets/Scripts/weaponController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class weaponController : MonoBehaviour
 {
+    public GameObject trails;
+    
+    private bool canAttack = true;
     private bool isStrafe = false;
     private Animator _anim;
 
@@ -12,6 +15,7 @@ public class weaponController : MonoBehaviour
     void Start()
     {
         _anim = GetComponent<Animator>();
+        trailClose();
     }
 
     // Update is called once per frame
@@ -23,27 +27,46 @@ public class weaponController : MonoBehaviour
             isStrafe = !isStrafe;
         }
 
+        if (Input.GetKeyDown(KeyCode.Mouse0) && isStrafe == true && canAttack == true)
+        {
+            _anim.SetTrigger("Saldır");
+        }
+        
         if (isStrafe == true)
         {
             GetComponent<characterController>().hareketTipi = characterController.MovementType.Strafe;
+            GetComponent<IKLook>().azal();
         }
         if (isStrafe == false)
         {
             GetComponent<characterController>().hareketTipi = characterController.MovementType.Directional;
+            GetComponent<IKLook>().art();
         }
+    }
+    void equip()
+    {
+        backSword.SetActive(false);
+        handSword.SetActive(true);
+    }
+    void unequip()
+    {
+        backSword.SetActive(true);
+        handSword.SetActive(false);
+    }
 
-        void equip()
+    public void trailOpen()
+    {
+        for (int i = 0; i < trails.transform.childCount; i++)
         {
-            backSword.SetActive(false);
-            handSword.SetActive(true);
+            trails.transform.GetChild(i).gameObject.GetComponent<TrailRenderer>().emitting = true;
         }
+    }
 
-        void unequip()
+    public void trailClose()
+    {
+        for (int i = 0; i < trails.transform.childCount; i++)
         {
-            backSword.SetActive(true);
-            handSword.SetActive(false);
+            trails.transform.GetChild(i).gameObject.GetComponent<TrailRenderer>().emitting = false;
         }
-        
-        
     }
 }
