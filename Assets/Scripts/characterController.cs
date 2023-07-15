f9ee0d652139faede8a576521b1438187e0877f7
Sercan Ümit Öner <117740824+lucanusx@users.noreply.github.com>
2023-07-16 00:36:32 +0300

    pre merge

--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 
 public class characterController : MonoBehaviour
 {
-    [Header("Metrics")]
-    public float damp;
+    [Header("Metrics")] public float damp;
     [Range(1, 20)] public float rotationSpeed;
     [Range(1, 20)] public float strafeTurnSpeed;
     float normalFov;
@@ -21,12 +20,8 @@ public class characterController : MonoBehaviour
     float maxSpeed;
     public KeyCode sprintButton = KeyCode.LeftShift;
     public KeyCode walkButton = KeyCode.C;
-   // public KeyCode jumpButton = KeyCode.Space;
-
-   bool canJump = true; 
-   public float jumpForce = 3;
-   // public float jumpCooldown = 1.2f;
-
+    public KeyCode jumpButton = KeyCode.Space;
+    public float jumpForce = 5f;
     public enum MovementType
     {
         Directional,
@@ -65,25 +60,26 @@ public class characterController : MonoBehaviour
 
     void movement()
     {
-        if (hareketTipi==MovementType.Strafe)
+        if (hareketTipi == MovementType.Strafe)
         {
             inputX = Input.GetAxis("Horizontal");
-            inputY = Input.GetAxis("Vertical");   
-            
-            _anim.SetFloat("iX",inputX,damp,Time.deltaTime*10);
-            _anim.SetFloat("iY",inputY,damp,Time.deltaTime*10);
+            inputY = Input.GetAxis("Vertical");
+
+            _anim.SetFloat("iX", inputX, damp, Time.deltaTime * 10);
+            _anim.SetFloat("iY", inputY, damp, Time.deltaTime * 10);
 
             var hareketEdiyor = inputX != 0 || inputY != 0;
 
             if (hareketEdiyor)
             {
                 float yawCamera = mainCam.transform.rotation.eulerAngles.y;
-                transform.rotation=Quaternion.Slerp(transform.rotation,Quaternion.Euler(0,yawCamera,0),strafeTurnSpeed*Time.fixedDeltaTime );
-                _anim.SetBool("strafeMoving",true);
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0),
+                    strafeTurnSpeed * Time.fixedDeltaTime);
+                _anim.SetBool("strafeMoving", true);
             }
             else
             {
-                _anim.SetBool("strafeMoving",false);
+                _anim.SetBool("strafeMoving", false);
             }
         }
 
@@ -106,65 +102,25 @@ public class characterController : MonoBehaviour
                 inputY = Input.GetAxis("Vertical");
             }
             else
-            { 
+            {
                 mainCam.fieldOfView = Mathf.Lerp(mainCam.fieldOfView, normalFov, Time.deltaTime * 2);
                 maxSpeed = 1;
                 inputX = Input.GetAxis("Horizontal");
                 inputY = Input.GetAxis("Vertical");
             }
-            
-            if (canJump && Input.GetKeyDown(KeyCode.Space))
+            if (Input.GetKeyDown(jumpButton))
             {
                 _anim.SetTrigger("jump");
-                jump();
+                Invoke("Jump", 0.8f);
             }
         }
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
-
-    void jump()
+    void Jump()
     {
-        canJump = false;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
+        if (Mathf.Abs(_rb.velocity.y) < 0.01f) // Karakter zıplama sırasında havadaysa tekrar zıplamaması için kontrol
         {
-            canJump = true;
+            _rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
         }
     }
-
-
-    /* void Jump()
-    {
-      //  _anim.SetTrigger("Jump");  Zıplama animasyonu trigger
-        _rb.AddForce(Vector3.up * 5, ForceMode.Impulse); 
-        canJump = false; 
-        Invoke(nameof(ResetJump), jumpCooldown); //resetjump icin bekleme suresi
-    }
-
-    void ResetJump()
-    {
-        canJump = true; 
-    }
-
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            canJump = true; 
-        }
-    }
-    */
-}
- 
+}    
